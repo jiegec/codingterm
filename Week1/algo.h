9dc7fbecc2481c42b7ffe86c32a6eafcbcf17fe6
Jiajie Chen <jiegec@qq.com>
2018-08-28 16:15:05 +0800

    Eliminate algo warnings

--- Week1/algo.h
+++ Week1/algo.h
@@ -1,22 +1,30 @@
 // Copyright (C) 2018 Jiajie Chen
-// 
+//
 // This file is part of Week1.
-// 
+//
 // Week1 is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
 // (at your option) any later version.
-// 
+//
 // Week1 is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 // GNU General Public License for more details.
-// 
+//
 // You should have received a copy of the GNU General Public License
 // along with Week1.  If not, see <http://www.gnu.org/licenses/>.
-// 
+//
 
 #ifndef ALGO_H
 #define ALGO_H
+
+#include <vector>
+
+//函数功能：计算芯片所有管道的液体流速
+//参数含义：num，芯片的网格边长；length，存储网格中每个管道的长度，若管道不存在用0表示；i1,i2,o1,o2,o3
+//				分别表示两个输入管道与三个输出管道在第几列。
+std::vector<double> caluconspeed(int num, std::vector<double> &length, int i1, int i2,
+                            int o1, int o2, int o3);
 
 #endif
