352f4e3b505e16d53cbb73602f463f0ef8bd6029
Jiajie Chen <jiegec@qq.com>
2018-08-30 13:44:48 +0800

    Fix Concentration algo 2

--- Week1/algo.cpp
+++ Week1/algo.cpp
@@ -586,7 +586,7 @@ vector<double> caluconspeed(int num, vector<double> &length, int i1, int i2,
         int lastedge = -1;
         for (int dir = 0; dir < 4; dir++) {
           int e = getdirline(i, j, dir);
-          if (e >= EDGESUM || edges[e].leng == 0)
+          if (e >= EDGESUM || edges[e].leng == 0 || abs(edges[e].v) < NAX)
             continue;
           if (edges[e].n1 == nodeno) {
             // outflow
