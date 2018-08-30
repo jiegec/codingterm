e9fd554287e968cd63d6a575c595dde4c2a76840
Jiajie Chen <jiegec@qq.com>
2018-08-30 13:29:06 +0800

    Ignore zero-flow edges in concentration calculation

--- Week1/algo.cpp
+++ Week1/algo.cpp
@@ -547,7 +547,7 @@ vector<double> caluconspeed(int num, vector<double> &length, int i1, int i2,
 
       for (int dir = 0; dir <= 3; dir++) {
         int e = getdirline(i, j, dir);
-        if (e >= EDGESUM || edges[e].leng == 0)
+        if (e >= EDGESUM || edges[e].leng == 0 || edges[e].v == 0)
           continue;
         if (edges[e].n1 == nodeno) {
           // outflow
