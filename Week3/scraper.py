cf68f138bf4e1146d5c55694c8d882bcb722686e
Jiajie Chen <jiegec@qq.com>
2018-09-13 05:56:14 +0800

    Refactor scraper

--- Week3/scraper.py
+++ Week3/scraper.py
@@ -9,7 +9,10 @@ from bs4 import BeautifulSoup
 import time
 import random
 from news.models import News
+import json
+import requests
 
+''' 
 import requests
 import logging
 
@@ -28,32 +31,17 @@ logging.getLogger().setLevel(logging.DEBUG)
 requests_log = logging.getLogger("urllib3")
 requests_log.setLevel(logging.DEBUG)
 requests_log.propagate = True
+'''
 
 s = requests.Session()
-try:
-    common_ua = [
-        'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/601.7.7 (KHTML, like Gecko) Version/9.1.2 Safari/601.7.7',
-        'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.109 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
-        'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:56.0) Gecko/20100101 Firefox/56.0',
-        'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36']
-    url = f'http://news.qq.com/'
-    headers = {
-        'User-Agent': common_ua[random.randint(0, len(common_ua)-1)],
-        'DNT': '1',
-        'Referer': 'http://news.qq.com',
-        'Accept': '*/*',
-        'Accept-Encoding': 'gzip, deflate',
-        'Accept-Language': 'zh-CH,zh;q=0.9,en;q=0.8',
-        'X-Requested-With': 'XMLHttpRequest'
-    }
+common_ua = [
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/601.7.7 (KHTML, like Gecko) Version/9.1.2 Safari/601.7.7',
+    'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.109 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
+    'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:56.0) Gecko/20100101 Firefox/56.0',
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36']
 
-    print(f'start to pull list from {url}')
-    r = s.get(url, headers=headers)
-    soup = BeautifulSoup(r.text, 'html.parser')
-    links = soup.find_all('a', class_='pic')
-    urls = [x['href'] for x in links]
-    print(f'Got {len(urls)} links')
+def go_scrape_links(urls):
     for url in urls:
         try:
             News.objects.get(url=url)
@@ -66,23 +54,15 @@ try:
                 time.sleep(timeout)
                 timeout = 2
             except KeyboardInterrupt:
-                break
+                raise
             except:
                 timeout = timeout * 2
                 pass
             pass
-except:
-    pass
 
-for kind in ['ent', 'sports', 'house', 'tech', 'auto', 'digi', 'fashion', 'astro', 'gamezone', 'cul', 'edu']:
+if True:
     try:
-        common_ua = [
-            'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/601.7.7 (KHTML, like Gecko) Version/9.1.2 Safari/601.7.7',
-            'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.109 Safari/537.36',
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/63.0.3239.132 Safari/537.36',
-            'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:56.0) Gecko/20100101 Firefox/56.0',
-            'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.89 Safari/537.36']
-        url = f'http://news.qq.com/ninja/timeline_{kind}tab_newsindex.htm'
+        url = f'http://news.qq.com/'
         headers = {
             'User-Agent': common_ua[random.randint(0, len(common_ua)-1)],
             'DNT': '1',
@@ -96,27 +76,60 @@ for kind in ['ent', 'sports', 'house', 'tech', 'auto', 'digi', 'fashion', 'astro
         print(f'start to pull list from {url}')
         r = s.get(url, headers=headers)
         soup = BeautifulSoup(r.text, 'html.parser')
-        links = soup.find_all('a', class_='linkto')
+        links = soup.find_all('a', class_='pic')
         urls = [x['href'] for x in links]
         print(f'Got {len(urls)} links')
-        for url in urls:
-            try:
-                News.objects.get(url=url)
-                print(f'Already scraped {url}, ignoring')
-                # already scraped
-            except News.DoesNotExist:
-                timeout = 2
-                try:
-                    views.scrape_url(url)
-                    time.sleep(timeout)
-                    timeout = 2
-                except KeyboardInterrupt:
-                    break
-                except:
-                    timeout = timeout * 2
-                    pass
-                pass
-    except KeyboardInterrupt:
-        break
+        go_scrape_links(urls)
     except:
         pass
+
+if True:
+    for site in ['news', 'ent', 'sports', 'finance', 'tech', 'games', 'auto', 'edu', 'house']:
+        try:
+            url = f'http://roll.news.qq.com/interface/cpcroll.php?callback=rollback&site={site}&mode=1&cata=&date={time.strftime("%Y-%m-%d")}&page=1&_={int(time.time()*1000)}'
+            headers = {
+                'User-Agent': common_ua[random.randint(0, len(common_ua)-1)],
+                'DNT': '1',
+                'Referer': 'http://news.qq.com/articleList/rolls/',
+                'Accept': '*/*',
+                'Accept-Encoding': 'gzip, deflate',
+                'Accept-Language': 'zh-CH,zh;q=0.9,en;q=0.8',
+                'X-Requested-With': 'XMLHttpRequest'
+            }
+
+            print(f'start to pull list from {url}')
+            r = s.get(url, headers=headers)
+            data = json.loads(r.text[len('rollback('):-1])
+            urls = [x['url'] for x in data['data']['article_info']]
+            print(f'Got {len(urls)} links')
+            go_scrape_links(urls)
+        except KeyboardInterrupt:
+            raise
+        except:
+            pass
+
+if True:
+    for kind in ['ent', 'sports', 'house', 'tech', 'auto', 'digi', 'fashion', 'astro', 'gamezone', 'cul', 'edu']:
+        try:
+            url = f'http://news.qq.com/ninja/timeline_{kind}tab_newsindex.htm'
+            headers = {
+                'User-Agent': common_ua[random.randint(0, len(common_ua)-1)],
+                'DNT': '1',
+                'Referer': 'http://news.qq.com',
+                'Accept': '*/*',
+                'Accept-Encoding': 'gzip, deflate',
+                'Accept-Language': 'zh-CH,zh;q=0.9,en;q=0.8',
+                'X-Requested-With': 'XMLHttpRequest'
+            }
+
+            print(f'start to pull list from {url}')
+            r = s.get(url, headers=headers)
+            soup = BeautifulSoup(r.text, 'html.parser')
+            links = soup.find_all('a', class_='linkto')
+            urls = [x['href'] for x in links]
+            print(f'Got {len(urls)} links')
+            go_scrape_links(urls)
+        except KeyboardInterrupt:
+            raise
+        except:
+            pass
